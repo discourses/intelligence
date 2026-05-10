723d4e3388efd5f1ecd4dbe939d04e95ba57dd7e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-05-10 17:17:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+
+  /* 
+    Load _env_ based on `mode` in the current working directory.  Set the third parameter 
+    to '' to load all _env_ regardless of the `VITE_` prefix.
+  */
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      open: false,
+      host: true,
+      port: env.APP_PORT ? Number(env.APP_PORT) : 5173
+    },
+    build: {
+      outDir: 'dist'
+    }
+  }
+  
 })
